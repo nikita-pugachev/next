f8fff003b71e83b9335afb047dada724c87b4999
Nikita Pugachev <RUSSSSing@yandex.ru>
2026-05-20 11:08:13 +0300

    feat: add component Input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-'use client';
 import styles from '@/app/app.module.scss';
 
 export default function Page() {
-    return (
-        <>
-        </>
-    );
+    return null;
 }
---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,46 @@
+import styles from "./Input.module.scss";
+import { FC } from "react";
+
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  id?: string;
+  type?: "text" | "email" | "password" | "name";
+  label?: string;
+  error?: string;
+  hint?: string;
+  icon?: React.ReactNode;
+  onClick?: () => void;
+}
+
+export const Input: FC<InputProps> = ({
+  id,
+  type = "text",
+  label,
+  error,
+  hint,
+  icon,
+  className,
+  onClick,
+  ...props
+}) => {
+  return (
+    <div className={styles.inputWrapper}>
+      {label && (
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <div className={styles.inputContainer}>
+        <input
+          className={`${styles.input} ${className}`}
+          id={id}
+          type={type}
+          {...props}
+        />
+        {icon && <button onClick={onClick} className={styles.iconContainer}>{icon}</button>}
+      </div>
+      {error && <span className={styles.error}>{error}</span>}
+      {hint && <span className={styles.hint}>{hint}</span>}
+    </div>
+  );
+};
